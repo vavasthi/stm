75ae33d7ecaecbc206a94c7b5c002f3136f3f176
Vinay Avasthi <vinay.avasthi@hp.com>
2016-07-02 12:19:47 +0530

    Starting server from scratch.

--- sanjnan-server/src/main/java/com/sanjnan/server/pojos/constants/DeviceOTAStatus.java
+++
@@ -1,9 +0,0 @@
-package com.sanjnan.server.pojos.constants;
-
-/**
- * Created by maheshsapre on 27/02/16.
- */
-public class DeviceOTAStatus {
-  public static final int UPGRADE_IN_PROGRESS = 1;
-  public static final int UPGRADE_FINISHED = 0;
-}
